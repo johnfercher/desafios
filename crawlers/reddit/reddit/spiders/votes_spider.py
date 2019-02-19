20ef4094d26d4fbb5f5e17ca6ff23b53b608d5f1
John Fercher <johnathanfercher22@gmail.com>
2019-02-18 22:24:40 -0300

    ok

--- crawlers/reddit/reddit/spiders/votes_spider.py
+++ crawlers/reddit/reddit/spiders/votes_spider.py
@@ -25,17 +25,23 @@ class VotesSpider(scrapy.Spider):
         scores = self.__get_all_scores(response)
         links = self.__get_all_links(response)
 
-        print(titles)
-        print(scores)
-        print(links)
+
+        for i in range(0, len(scores)):
+            if int(scores[i]) >= 5000:
+                print(titles[i])
+                print(scores[i])
+                print(links[i])
+                print(" ")
 
     def __get_all_titles(self, response):
         titles_xpath = response.xpath('//div//div//p//a[contains(@class, "title may-blank")]/text()')
         return [title_xpath.get() for title_xpath in titles_xpath]
 
     def __get_all_scores(self, response):
-        scores_xpath = response.xpath('//div//div[@class="score likes"]/text()')
-        return [score_xpath.get() for score_xpath in scores_xpath]
+        scores_xpath = response.xpath('//div//div[@class="score likes"]')
+
+
+        return [score_xpath.xpath("@title").get() for score_xpath in scores_xpath]
 
     def __get_all_links(self, response):
         links_xpath = response.xpath('//div//div//div//div//p//a[contains(@class, "title may-blank")]/@href')
